fe8e6d168dddfdba0a7bbcea318759d603a17484
John Heinnickel <john.heinnickel@thermofisher.com>
2022-05-22 07:29:39 -0700

    IO-490 - Patch scripts\n\nIO-490 - Apply first update\n

--- build_me.sh
+++ build_me.sh
@@ -1,20 +1,47 @@
 #!/bin/bash
 
-history="hist_size.csv"
+history="./hist_size.csv"
 
 basedir="$(basename `pwd`)"
-artifact="$(echo $basedir | sed 's/pseudo-//')"
-state_now="$(cat state_now.dat)"
 
-for line in `grep '^${artifact}' "${history}"`
+# Strip csd-genexus- and psuedo- prefixes away from working directory name
+# to get the artifact name of project being build
+artifact="$(echo $basedir | sed 's/csd-genexus-//' | sed 's/pseudo-//')"
+echo "${artifact}"
+
+# Inspect checked in state file to understand what version we are pretending
+# to be building a component of the bundle for.
+state_now="$(cat state_now.dat)"
+echo "${state_now}"
+
+# Find the line for the component that will belong in the bundle whose
+# version was given by ${state_now} and pull it by FTP, simulating a genuine
+# build.
+for line in `grep "^${artifact}" "${history}"`
 do
-	match_to="$(echo $line | awk -F, '{print $6}')"
-	if [${match_to} == ${state_now}]
+	echo "${line}"
+	match_to="$(echo $line | awk -F, '{print $8}')"
+	echo "${match_to}"
+	if [[ ${match_to} == ${state_now} ]]
 	then
-		url="$(echo $line | awk -F, '{ print "http://nfs.ssfisilon2.itw/Valkyrie/AssayDev/TSDx/AssayDev/updates/"$1"_"$2"_"$3".deb" }')"
+		url="$(echo $line | awk -F, '{ print "http://lemon.itw/"$4"/TSDx/AssayDev/updates/"$1"_"$2"_"$3".deb" }')"
 		wget "${url}"
 		file="$(basename ${url})"
-		echo $file > output.name
+		file_name_length="$(($(echo ${file} | wc -c) - 1))"
+		build_output_name="$(echo "${file}" | head -"${file_name_length}c" | tee build_output_name.dat)"
+		# echo "${file}" | head -${file_name_length}c | tee output.name
+		cat > uploadBuildSpec.yaml << EOF
+{
+    "files": [
+        {
+            "pattern": "./${build_output_name}",
+	    "target": "csd-genexus-debian-dev/${build_output_name}",
+            "props": "test_prop=test_value;magic=wand;deb.distribution=bionic;deb.component=main;deb.architecture=amd64"
+        }
+    ]
+}
+EOF
+
 		exit 0
 	fi
 done
